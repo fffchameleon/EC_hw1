cff909b9e405ffe6e0882dddbf2d34d99408dcd2
cyxbla <snake96274@gapp.nthu.edu.tw>
2023-03-31 00:47:44 +0800

    v4.0.1

--- plot/plot.py
+++ plot/plot.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 folder_path = 'data'
-output_file = 'show/crossover_point.png'
-filename_pattern = 'avgfitness_n_point'
+output_file = 'show/tournament.png'
+filename_pattern = 'avgfitness_p_select'
 csv_files = [f for f in os.listdir(folder_path) if f.startswith(filename_pattern)]
 
 plt.figure(figsize=(12, 8))
@@ -17,9 +17,9 @@ for file in csv_files:
     var = int(file.split('_')[-1].split('.')[0])
 
     plt.plot(data['Binary'], label=f'binary_{var}')
-    # plt.plot(data['Real'], label=f'real_{var}')
+    plt.plot(data['Real'], label=f'real_{var}')
 
-plt.title('Independent variable: n-point crossover')
+plt.title('Independent variable: tournament select n')
 plt.xlabel('Generation')
 plt.ylabel('Avg Fitness (30 trials)')
 plt.legend()
